a943428c104e469fb970f50eb324086ada382f77
Mike Gehard <mike.gehard@gmail.com>
2016-02-15 18:28:22 -0700

    More homework

---
+++ chapter11/Programmers.hs
@@ -0,0 +1,32 @@
+module Programmers where
+    data OperatingSystem =
+           GnuPlusLinux
+           | OpenBSDPlusNevermindJustBSDStill | Mac
+           | Windows
+           deriving (Eq, Show)
+
+    data ProgrammingLanguage =
+        Haskell
+        | Agda
+        | Idris
+        | PureScript deriving (Eq, Show)
+
+    data Programmer =
+        Programmer { os :: OperatingSystem
+        , lang :: ProgrammingLanguage }
+        deriving (Eq, Show)
+
+    allOperatingSystems :: [OperatingSystem]
+    allOperatingSystems =
+        [ GnuPlusLinux
+        , OpenBSDPlusNevermindJustBSDStill , Mac
+        , Windows
+        ]
+
+    allLanguages :: [ProgrammingLanguage]
+    allLanguages = [Haskell, Agda, Idris, PureScript]
+
+    allProgrammers :: [Programmer]
+    allProgrammers = [Programmer os lang | os <- allOperatingSystems, lang <- allLanguages]
+
+
